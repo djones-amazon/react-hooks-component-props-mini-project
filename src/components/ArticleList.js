17f88ab072bee073089f9f8e903be29d705ba313
Devin Jones <devin.jones21083@gmail.com>
2023-05-27 22:47:40 -0400

    All deliverables complete. not stretch

---
+++ src/components/ArticleList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Article from "./Article.js";
+
+
+function ArticleList({posts}) {
+    
+    const postList = posts.map((post) => {
+        return (
+            <Article key={post.id} title={post.title} date={post.date} preview={post.preview} />
+        );
+    });
+    
+    return (
+        <main>
+            {postList}
+        </main>
+    )
+}
+
+export default ArticleList
